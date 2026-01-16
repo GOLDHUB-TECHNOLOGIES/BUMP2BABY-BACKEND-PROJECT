10dde290f6f7c2ddeae045e1573a60316519c75a
Ekerin Umar <99878825+umzzy@users.noreply.github.com>
2026-01-16 21:25:40 +0100

    Fixed validation

--- src/validation/authValidation.js
+++ src/validation/authValidation.js
@@ -3,6 +3,9 @@ import Joi from "joi";
 export const registerValidation = (data) => {
   const schema = Joi.object({
     role: Joi.string().valid("pregnant", "new_parent", "caregiver").required(),
+    babyage: Joi.string()
+      .valid("Newborn", "1mo", "2mo", "3mo", "4mo", "5mo", "6mo", "9mo", "12mo")
+      .required(),
     name: Joi.string().min(3).required(),
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
